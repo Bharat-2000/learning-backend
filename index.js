54006efe53c8c2d3398c1ad4d18de3101a875511
Bharat-2000 <yadeep999@gmail.com>
2023-05-16 15:47:06 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import path from 'path';
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
 
+const connection_url = "mongodb+srv://bharatc_2000:<password>@cluster0.hqd0ufs.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect('mongodb://127.0.0.1:27017', {
     dbName: 'mongodb'
 }).then(() => { console.log('Database connected!') }).catch((error) => { console.log(error.message) });
@@ -18,18 +20,42 @@ const Message = mongoose.model("Message", messageSchema);
 
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    // const pathlocation = path.resolve();
-    // res.sendFile(path.join(pathlocation, './index.html'));
-    res.render("index", { name: 'bharat' });
+const isAuthenticated = (req, res, next) => {
+    const { token } = req.cookies;
+    if (token) {
+        next();
+    }
+    else {
+        res.render('login');
+    }
+}
+
+app.get('/', isAuthenticated, (req, res) => {
+    res.render('logout');
 });
 
+app.post("/login", (req, res) => {
+    res.cookie("token", "login successful!", {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000)
+    });
+    res.redirect("/");
+});
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now())
+    });
+    res.redirect('/');
+})
+
 app.post('/', async (req, res) => {
-    const {name, email} = req.body;
-    await Message.create({ name, email});
+    const { name, email } = req.body;
+    await Message.create({ name, email });
 });
 
 app.get('/users', (req, res) => {
